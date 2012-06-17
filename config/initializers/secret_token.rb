f930c3560624c2aedc328aa9fd0c7024941d5472
opensuse <opensuse@linux-6hjg.site>
2012-06-17 22:34:43 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '39a913094ddc8c4064d5fc7675157d83d32b58d84a7328308013f1acd04753ac6fa58938266d89b58fade44059fd1f8a4e6dab3c47cd850de641912e60293ee8'
